2773dc7203a3849fc94fdb4711e2edf198cfbbca
VictorGM01 <victormarques8801@gmail.com>
2023-05-11 08:43:47 -0300

    feat: adiciona endpoint de delete e comentários no código

--- src/routes/turmaRoutes.js
+++ src/routes/turmaRoutes.js
@@ -1,6 +1,10 @@
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+
+// Essa função é responável por ler a rota inserida e, a partir dela, direcionar para um controlador
+// Ex.: se a url for /turmas, essa função direciona para o controlador listaTurmas.
+// Para além, os métodos http também são considerados, como get e post, por exemplo.
 module.exports = function(application){
     // retorna controlador das turmas
     application.get('/turmas', function(req, res){
@@ -19,6 +23,13 @@ module.exports = function(application){
     // retorna controlador para a atualização da turma
     application.post('/turma/atualizar', urlencodedParser, function(req, res){
       application.src.controllers.turmaControllers.atualiza(
+        application, req, res
+      );
+    });
+
+    // retorna controlador para deletar a turma
+    application.get('/turma/deletar', urlencodedParser, function(req, res){
+      application.src.controllers.turmaControllers.deleta(
         application, req, res
       );
     });
