b553b45f0e67324498b5951f00a9ead9246fd39f
VictorGM01 <victormarques8801@gmail.com>
2023-05-10 22:26:52 -0300

    feat: adiciona banco de dados e endpoints relacionados à turma

---
+++ src/models/turmaModels.js
@@ -0,0 +1,91 @@
+const database = require('../data/data')
+
+
+function turmas() {}
+
+// modelo responsável por fazer a atualização da turma
+// o id e o nome da turma são passados via corpo da requisição.
+turmas.prototype.updateTurma = function(callback, idTurma, nomeTurma) {
+    var sql = 'UPDATE turma set nome = "' + nomeTurma + '"' + 
+    'WHERE id_turma = ' + idTurma;
+
+    // executa a atualização e verifica se houve algum erro
+    database.appDB.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            callback(err.message)
+        }
+        callback('Turma atualizada')
+    });
+}
+
+// modelo responsável por fazer a consulta das turmas de acordo com o id do professor
+// esse id é passado via url. Ex.: turmas?idProfessor=1
+turmas.prototype.getProfTurmas = function(callback, idProf) {
+    var sql = 'SELECT * FROM turma WHERE id_professor = ' + idProf;
+
+    // executa a consulta sql e retorna os dados na função callback, a qual será usada
+    // no controlador para mostrar os dados na página.
+    database.appDB.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            }
+        callback(rows)
+    });
+}
+
+// modelo responsável por fazer a listagem de todos os alunos da turma.
+// o id é obtido via url. Ex.: turmas?idTurma=1
+turmas.prototype.getTurmaAlunos = function(callback, idTurma) {
+
+    // essa consulta une as tabelas aluno, aluno_turma e turma para trazer os dados
+    // dos alunos da turma do id especificado
+    var sql = 'SELECT aluno.id_aluno, aluno.nome as nome_aluno, turma.id_turma, turma.nome as nome_turma ' +
+    'FROM aluno_turma ' +
+    'JOIN aluno ON aluno_turma.id_aluno = aluno.id_aluno ' +
+    'JOIN turma ON aluno_turma.id_turma = turma.id_turma ' +
+    'WHERE aluno_turma.id_turma = ' + idTurma;
+
+    // retorno dos dados
+    database.appDB.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            }
+        callback(rows)
+    });
+}
+
+// modelo responsável por criar uma turma e criar o relacionamento turma_disciplina logo em seguida
+turmas.prototype.postTurma = function(callback, idProfessor, idDisciplina, nomeTurma, serieTurma) {
+
+    // nesse ponto, a turma é criada com o nome, serie e id do professor
+    // passados via corpo da requisição
+    var sql = 'INSERT INTO turma (nome, serie, id_professor) VALUES ( "' + 
+    nomeTurma + '",' + serieTurma + ',' + idProfessor + ');';
+    var erro = ''
+    database.appDB.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            callback(err.message);
+            erro = err; // caso haja erro na inserção, ele é inserido na variável erro
+        }
+    });
+
+    // se o tamanho da variável for menor que 1, significa que a variável está vazia
+    // logo, não houve erro na inserção e, por isso, pode criar o relacionamento
+    if(erro.length < 1){
+        sql = 'INSERT INTO turma_disciplina VALUES ((SELECT id_turma FROM turma WHERE nome = "' + nomeTurma +
+        '" AND id_professor = ' + idProfessor + '), ' + idDisciplina + ');';
+        database.appDB.all(sql, [], (err, rows) => {
+            if (err) {
+                console.error(err.message);
+                callback(err.message)
+            }
+            callback("turma criada e vinculada à disciplina")
+        });
+    };
+}
+
+module.exports = function(){
+    return turmas;
+}
