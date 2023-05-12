a71ced9abd79bcdc8ee31d3ed84edaa40b09ea83
VictorGM01 <victormarques8801@gmail.com>
2023-05-12 17:31:29 -0300

    docs: atualiza estruturação de pastas

--- src/data/banco.sql
+++ src/data/banco.sql
@@ -17,31 +17,31 @@ CREATE TABLE IF NOT EXISTS "avaliacao" (
 	"data"	TEXT NOT NULL,
 	"serie"	INTEGER NOT NULL,
 	"id_professor"	INTEGER NOT NULL,
-	PRIMARY KEY("id_avaliacao" AUTOINCREMENT),
-	FOREIGN KEY("id_professor") REFERENCES "professor"("id_professor") ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY("id_professor") REFERENCES "professor"("id_professor") ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY("id_avaliacao" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "area_conhecimento" (
 	"id_area"	INTEGER NOT NULL,
 	"nome"	TEXT NOT NULL,
 	"id_disciplina"	INTEGER NOT NULL,
-	PRIMARY KEY("id_area" AUTOINCREMENT),
-	FOREIGN KEY("id_disciplina") REFERENCES "disciplina"("id_disciplina") ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY("id_disciplina") REFERENCES "disciplina"("id_disciplina") ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY("id_area" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "bloco_questao" (
 	"id_avaliacao"	INTEGER NOT NULL,
 	"num_bloco"	INTEGER NOT NULL,
 	"quant_questoes"	INTEGER NOT NULL,
 	"id_area"	INTEGER NOT NULL,
-	PRIMARY KEY("id_avaliacao","num_bloco"),
+	FOREIGN KEY("id_area") REFERENCES "area_conhecimento"("id_area") ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY("id_avaliacao") REFERENCES "avaliacao"("id_avaliacao"),
-	FOREIGN KEY("id_area") REFERENCES "area_conhecimento"("id_area") ON DELETE CASCADE ON UPDATE CASCADE
+	PRIMARY KEY("id_avaliacao","num_bloco")
 );
 CREATE TABLE IF NOT EXISTS "prof_disciplina" (
 	"id_professor"	INTEGER NOT NULL,
 	"id_disciplina"	INTEGER NOT NULL,
-	PRIMARY KEY("id_professor","id_disciplina"),
+	FOREIGN KEY("id_professor") REFERENCES "professor"("id_professor") ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY("id_disciplina") REFERENCES "disciplina"("id_disciplina") ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY("id_professor") REFERENCES "professor"("id_professor") ON DELETE CASCADE ON UPDATE CASCADE
+	PRIMARY KEY("id_professor","id_disciplina")
 );
 CREATE TABLE IF NOT EXISTS "nota" (
 	"id_aluno"	INTEGER NOT NULL,
@@ -49,10 +49,10 @@ CREATE TABLE IF NOT EXISTS "nota" (
 	"num_bloco"	INTEGER NOT NULL,
 	"nota_acertos"	INTEGER NOT NULL,
 	"data"	TEXT NOT NULL,
-	PRIMARY KEY("id_aluno","id_avaliacao","num_bloco","nota_acertos"),
-	FOREIGN KEY("id_avaliacao") REFERENCES "avaliacao"("id_avaliacao") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("id_aluno") REFERENCES "aluno"("id_aluno") ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY("num_bloco") REFERENCES "bloco_questao"("num_bloco") ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY("id_aluno") REFERENCES "aluno"("id_aluno") ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY("id_avaliacao") REFERENCES "avaliacao"("id_avaliacao") ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY("id_aluno","id_avaliacao","num_bloco","nota_acertos")
 );
 CREATE TABLE IF NOT EXISTS "aluno" (
 	"id_aluno"	INTEGER NOT NULL,
@@ -65,8 +65,8 @@ CREATE TABLE IF NOT EXISTS "aluno" (
 CREATE TABLE IF NOT EXISTS "aluno_turma" (
 	"id_aluno"	INTEGER NOT NULL,
 	"id_turma"	INTEGER NOT NULL,
-	FOREIGN KEY("id_turma") REFERENCES "turma"("id_turma") ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY("id_aluno") REFERENCES "aluno"("id_aluno") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("id_turma") REFERENCES "turma"("id_turma") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("id_aluno","id_turma")
 );
 CREATE TABLE IF NOT EXISTS "turma" (
@@ -83,8 +83,8 @@ INSERT INTO "professor" VALUES (1,'Ana Silva','senha1','ana.silva@exemplo.com');
 INSERT INTO "professor" VALUES (2,'João Souza','senha2','joao.souza@exemplo.com');
 INSERT INTO "professor" VALUES (3,'Maria Santos','senha3','maria.santos@exemplo.com');
 INSERT INTO "professor" VALUES (4,'Pedro Almeida','senha4','pedro.almeida@exemplo.com');
-INSERT INTO "professor" VALUES (5,'Julia Oliveira','senha5','julia.oliveira@exemplo.com');
 INSERT INTO "disciplina" VALUES (1,'matemática');
+INSERT INTO "avaliacao" VALUES (1,'Teste 2.0','10-05-2023',7,1);
 INSERT INTO "aluno" VALUES (1,'Carolos Santos',6,1);
 INSERT INTO "aluno" VALUES (2,'Julia Oliveira',6,1);
 INSERT INTO "aluno" VALUES (3,'Pedro Almeida',6,1);
@@ -95,6 +95,7 @@ INSERT INTO "aluno" VALUES (7,'Fernando Oliveira',7,2);
 INSERT INTO "aluno" VALUES (8,'Mariana Almeida',7,2);
 INSERT INTO "aluno" VALUES (9,'Luis Souza',7,2);
 INSERT INTO "aluno" VALUES (10,'Sara Silva',7,2);
+INSERT INTO "aluno" VALUES (11,'Marcelao 3.0',9,2);
 INSERT INTO "aluno_turma" VALUES (1,1);
 INSERT INTO "aluno_turma" VALUES (2,1);
 INSERT INTO "aluno_turma" VALUES (3,1);
@@ -115,5 +116,4 @@ INSERT INTO "turma" VALUES (7,'8c',8,1,1);
 INSERT INTO "turma" VALUES (8,'8d',8,2,1);
 INSERT INTO "turma" VALUES (9,'8d',8,2,1);
 INSERT INTO "turma" VALUES (10,'9e',9,2,1);
-INSERT INTO "turma" VALUES (11,'9c',9,2,1);
 COMMIT;
