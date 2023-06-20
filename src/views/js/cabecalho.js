281a2bb690538146c5bf8720705bf423655567d1
VictorGM01 <victormarques8801@gmail.com>
2023-06-20 15:53:55 -0300

    feat: adiciona responsividade do cabeçalho com jquery

--- src/views/js/cabecalho.js
+++ src/views/js/cabecalho.js
@@ -7,4 +7,22 @@ $(document).ready(function(){
 			$("main").show();
 		}
 	});
+
+	function ajustarTamanhoTela(mediaQuery) {
+        if (mediaQuery.matches) {
+          $(".nav_desktop").css("display", "flex");
+          $(".nav").css("display", "none");
+        } else {
+          $(".nav_desktop").css("display", "none");
+          $(".nav").css("display", "flex");
+        }
+      }
+      
+      var mediaQuery = window.matchMedia("(min-width: 1000px)");
+      ajustarTamanhoTela(mediaQuery); // Chamada inicial
+      
+      mediaQuery.addEventListener("change", function(event) {
+        ajustarTamanhoTela(event.target);
+      });    
+    
 });
--- src/views/js/modoEscuro.js
+++ src/views/js/modoEscuro.js
@@ -9,13 +9,23 @@ $(document).ready(() => {
         }
     })
 
+    $('#caixa_alternador-desktop').change(() => {
+        alternarTema()
+
+        localStorage.removeItem('darkMode');
+
+        if ($(':root').hasClass('darkMode')) {
+            localStorage.setItem('darkMode', 1);
+        }
+    })
+
     function alternarTema() {
         $(':root').toggleClass('darkMode');
     }
 
     function carregarTema() {
         if (localStorage.getItem('darkMode')) {
-            $('#caixa_alternador').click();
+            $('#caixa_alternador-desktop').click();
         }
     }
     carregarTema()
